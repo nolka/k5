853c0e28f11cbc4a622f5a72c8314a3913a6c6dc
xternalx <i@xternalx.com>
2011-09-15 02:15:52 +0800

    Mysql updated

--- System/classes/class.Database.php
+++ System/classes/class.Database.php
@@ -1,10 +1,17 @@
 <?php
 
 class DatabaseConnectionException extends Exception {}
+class DatabaseQueryException extends Exception {}
+class DatabaseFetchMode
+{
+    const Assoc = 0;
+    const Object = 1;
+}
 
 class Database
 {
     private $res = null;
+    private $queryRes = null;
     
     private $logMessages = array();
     
@@ -12,6 +19,9 @@ class Database
     private $rawQueryLen = 0;
     private $charId = -1;
     
+    private $fetchMode = DatabaseFetchMode::Assoc;
+    private $fetchClass = null;
+    private $fetchClassArgs = null;
     
     private $preparedQuery = array();
     
@@ -20,7 +30,7 @@ class Database
         $this->res = mysqli_connect($host, $username, $password, $dbname);
         if(mysqli_error($this->res))
         {
-            throw new DatabaseConnectionException("Error occurred when i try to connect to database: (".mysqli_errno().") ".mysqli_error());
+            throw new DatabaseConnectionException("Error occurred when i try to connect to database: (".mysqli_errno($this->res).") ".mysqli_error($this->res));
         }
         
     }
@@ -37,11 +47,12 @@ class Database
             $query_string = call_user_func_array(array($this, 'Prepare'), func_get_args());
         }
         $this->toLog('Prepared query: '.$query_string);
-    }
-    
-    private function toLog($message)
-    {
-        $this->logMessages[] = $message;
+        $this->queryRes = mysqli_query($this->res, $query_string);
+        if(!$this->queryRes)
+        {
+            throw new DatabaseQueryException("Database query failed: (".mysqli_errno($this->res).") ".mysqli_error($this->res)."<br />".$this->LogAsHtml());
+        }
+        return $this;
     }
     
     public function LogAsHtml()
@@ -56,7 +67,7 @@ class Database
     
     function Select($what)
     {
-        $this->rawQuery = "SELECT ".$this->escape($what);
+        $this->rawQuery = "SELECT SQL_CALC_FOUND_ROWS ".$this->escape($what);
         return $this;
     }
     
@@ -80,9 +91,9 @@ class Database
             else
             {
                 $this->rawQuery .= "`".$this->escape($k)."`=";
-                if(is_int($v) || is_float($v))
+                if(is_int($v) || is_float($v) || is_numeric($v))
                 {
-                    $this->rawQuery .= "'".$this->escape($v);
+                    $this->rawQuery .= $this->escape($v);
                 }
                 else
                 {
@@ -92,9 +103,21 @@ class Database
             $i++;
             if($i< $filtersCount)
             {
-                $this->rawQuery .= ", AND ";
+                $this->rawQuery .= " AND ";
             }
         }
+        return $this;
+    }
+    
+    /**
+     *
+     * @param mixed $what Field to order
+     * @param string $order ASC or DESC
+     * @return Database 
+     */
+    function OrderBy($what, $order = "ASC")
+    {
+        $this->rawQuery .= " ORDER BY `{$this->escape($what)}` {$this->escape($order)}";
         return $this;
     }
     
@@ -115,19 +138,111 @@ class Database
             $query = $this->rawQuery;
         }
         $this->toLog("Prepared query: ". $query);
-        $this->Query($query);
+        return $this->Query($query);
     }
     
-    function Assoc()
+    function Assoc($table_name)
     {
-        
+        return $this->Select("*")->From($table_name);
     }
     
-    function Object()
+    function Object($class, $constructor_args = null)
     {
-        
+        $this->fetchClassArgs =  $constructor_args;
+        if(is_object($class))
+        {
+            $class = get_class($class);
+        }
+        if(func_num_args()>1)
+        {
+            $args = func_get_args();
+            array_shift($args);
+        }
+        $this->fetchClass = $class;
+        return $this->Select("*")->From($class);
+    }
+    
+    function getItems($one = true, $sort_order = "ASC", $order_by = null)
+    {
+        if($order_by !== null && $sort_order !== null)
+        {
+            if($one)
+            {
+                $this->OrderBy($order_by, $sort_order)->Limit(1);
+            }
+            else
+            {
+                $this->OrderBy($order_by, $sort_order);
+            }
+        }
+        else
+        {
+            if($one)
+                $this->Limit(1);
+        }
+        $queryResult = $this->Query($this->rawQuery);
+        $dataResult = null;
+        if($this->fetchMode == DatabaseFetchMode::Assoc)
+        {
+            if($one)
+            {
+                $dataResult = mysqli_fetch_assoc($this->queryRes);
+            }
+            else
+            {
+                $dataResult = array();
+                while($res = mysqli_fetch_assoc($this->queryRes))
+                {
+                    $dataResult[] = $res;
+                }
+            }
+            $this->toLog(json_encode($dataResult));
+        }
+        else
+        {
+            if($one)
+            {
+                $dataResult = mysqli_fetch_object($this->queryRes, $this->fetchClass, $this->fetchClassArgs);
+            }
+            else
+            {
+                $dataResult = array();
+                while($res = mysqli_fetch_object($this->queryRes, $this->fetchClass, $this->fetchClassArgs));
+                {
+                    $dataResult[] = $res;
+                }
+            }
+            $this->toLog(json_encode($dataResult));
+        }
+        return $dataResult;
+    }
+    
+    function First($order_by = null)
+    {
+        return $this->getItems(true, "ASC", $this->escape($order_by));
+    }
+    
+    function Last($order_by = null)
+    {
+        return $this->getItems(true, "DESC", $this->escape($order_by));
+    }
+    
+    function All($order_by = null)
+    {
+        return $this->getItems(false, null, ($order_by === null)? null:$this->escape($order_by));
     }
 
+    
+    function AsObject($class_name)
+    {
+        $this->fetchMode = DatabaseFetchMode::Object;
+        #return 
+    }
+    
+    function AsArray()
+    {
+        $this->fetchMode = DatabaseFetchMode::Assoc;
+    }
 
     public function Prepare($query_string)
     {
@@ -153,6 +268,11 @@ class Database
                 {
                     $this->preparedQuery[] = floatval($this->escape(func_get_arg($argOffset++)));
                     $this->getNextChar();
+                }else
+                if($nextchar == "k")
+                {
+                    $this->preparedQuery[] = "`".($this->escape(func_get_arg($argOffset++)))."`";
+                    $this->getNextChar();
                 }
                 else
                 {
@@ -161,6 +281,11 @@ class Database
             }
         }
         return implode('', $this->preparedQuery);
+    }
+    
+    private function toLog($message)
+    {
+        $this->logMessages[] = $message;
     }
     
     private function escape($value)
@@ -212,6 +337,12 @@ class Database
     public function FieldsCount()
     {
         return mysqli_field_count($this->res);
+    }
+    
+    public function Close()
+    {
+        mysqli_free_result($this->queryRes);
+        mysqli_close($this->res);
     }
     
 }
